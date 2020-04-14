f7ce3d889f0404738ae411aacca569c3367a79d0
Alejandro <achdevops@gmail.com>
2020-04-14 16:44:19 -0300

    some validation and clean the test

--- az_vm_create.sh
+++ az_vm_create.sh
@@ -4,33 +4,45 @@
 # 1.- do "az login" and configure - we need to have a rg created
 # 2.- define the vars: rg_name / vm_name  / vm_image / zone
 
-echo "input rg name (already created): "
+echo "input rg name (to create or delete): "
 read rg_name
 
-echo "input vm name: "
+echo "input vm name (to create or delete): "
 read vm_name
 
-echo "input zone name[centralus]: "
+echo "input zone name *skip if you want delete resources*[centralus]: "
 read zone
 
-echo "input image [UbuntuLTS]: "
+echo "input image *skip if you want delete resources*[UbuntuLTS]: "
 read image
 
-echo "creating the rg ..."
-# create az resource group
-az group create --name $rg_name --location $zone
+while true; do
+  echo "do you want to delete existing resorces [y/n]:"
+  read input_user
+  
+  if [[ $input_user == "y" ]] || [[ $input_user == "n" ]]
+    then
+        if [[ "$input_user" == "y" ]]; then
+            az vm delete --name $vm_name -g $rg_name
+            az group delete --name $rg_name
+            break
+        else
+            echo "creating the rg ..."
+            # create az resource group
+            az group create --name $rg_name --location $zone
 
-echo "creating the vm..."
-az vm create --resource-group $rg_name \
-  --name $vm_name \
-  --image $image \
-  --generate-ssh-keys \
-  --output json \
-  --verbose
+            echo "creating the vm..."
+            az vm create --resource-group $rg_name \
+                --name $vm_name \
+                --image $image \
+                --generate-ssh-keys \
+                --output json \
+                --verbose
 
-# show the vm info
-my_ip=$(az vm show --name $vm_name --resource-group $rg_name --query publicIpAddress -o tsv)
-echo $my_ip
-#clean all
-#az vm delete --name $vm_name -g $rg_name
-#az group delete --name $rg_name
+                # show the vm info
+            my_ip=$(az vm show --name $vm_name --resource-group $rg_name --query publicIpAddress -o tsv)
+            echo $my_ip
+            break
+         fi
+    fi
+done
